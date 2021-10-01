62e9d72075926795d40eee763b06071dc7af309e
= <ariosvila@gmail.com>
2021-10-01 10:26:12 +0200

    Included validation process

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import cv2
 import os
 import sys
 import numpy as np
+import random
+import itertools
 
 CONST_IMG_DIR = "Data/PAGES/IMG/"
 CONST_AGNOSTIC_DIR = "Data/PAGES/AGNOSTIC/"
@@ -19,9 +21,41 @@ def load_data():
         X.append(cv2.imread(f"{CONST_IMG_DIR}{file}", 0))
         with open(f"{CONST_AGNOSTIC_DIR}{file.split('.')[0]}.agnostic") as agnosticfile:
             line = agnosticfile.readline()
-            Y.append(line.split(" "))
+            Y.append(['<sos>'] + line.split(" ") + ['<eos>'])
     
     return X, Y
+
+def validateModel(model, X, Y, i2w):
+    acc_ed_ser = 0
+    acc_len_ser = 0
+
+    randomindex = random.randint(0, len(X)-1)
+
+    for i in range(len(X)):
+        pred = model.predict(np.expand_dims(np.expand_dims(X[i],axis=0),axis=-1))[0]
+
+        out_best = np.argmax(pred,axis=1)
+
+        # Greedy decoding (TODO Cambiar por la funcion analoga del backend de keras)
+        out_best = [k for k, g in itertools.groupby(list(out_best))]
+        decoded = []
+        for c in out_best:
+            if c < len(i2w):  # CTC Blank must be ignored
+                decoded.append(i2w[c])
+
+        decoded.append('<eos>')
+        groundtruth = [i2w[label] for label in Y[i]]
+
+        if(i == randomindex):
+            print(f"Prediction - {decoded}")
+            print(f"True - {groundtruth}")
+
+        acc_len_ser += len(Y[i])
+        acc_ed_ser += levenshtein(decoded, groundtruth)
+
+
+    ser = 100. * acc_ed_ser / acc_len_ser
+    return ser
 
 
 def main():
@@ -67,12 +101,23 @@ def main():
                  }
     
     outputs = {'ctc': np.zeros([len(X_train)])}
+    
     best_ser = 10000
+    not_improved = 0
 
     for super_epoch in range(10000):
        model_train.fit(inputs,outputs, batch_size = 16, epochs = 5, verbose = 2)
-
-    pass
+       SER = validateModel(model_pred, XTest, YTest, i2w)
+       print(f"EPOCH {super_epoch} | SER {SER}")
+       if SER < best_ser:
+           print("SER improved - Saving epoch")
+           model_pred.save(f"SPAN_OMR.h5")
+           best_ser = SER
+           not_improved = 0
+       else:
+           not_improved += 1
+           if not_improved == 5:
+               break
 
 if __name__ == "__main__":
     main()
